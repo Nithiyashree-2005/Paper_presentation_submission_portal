e55a91ccd2d646baf6ef20a8eb9f9842b674d7d7
Nithiyashree-2005 <karthikeyaniveera1976@gmail.com>
2025-06-06 13:43:53 +0530

    Add files via upload

---
+++ NITHIYA/nithiya_database.sql
@@ -0,0 +1,17 @@
+create database paperdb;
+use paperdb;
+CREATE TABLE submission (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(255),
+    student_id VARCHAR(50),
+    email VARCHAR(255),
+    college VARCHAR(255),
+    stream VARCHAR(100),
+    course VARCHAR(100),
+    title VARCHAR(255),
+    abstract TEXT,
+    doc_link TEXT,
+    doc_upload_path TEXT,
+    status VARCHAR(50)
+);
+select * from submission;
